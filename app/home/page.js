20544a9e44da7f8c10d5f8435d931a01231d6a89
chetna <chetnagarg019@gmail.com>
2025-11-05 23:40:14 +0530

    second commit

--- app/home/page.js
+++ app/home/page.js
@@ -1,150 +1,3 @@
-// "use client";
-// import { useEffect, useState } from "react";
-// import { X } from "lucide-react";
-
-// export default function HomePage() {
-//   const [users, setUsers] = useState([]);
-//   const [search, setSearch] = useState("");
-//   const [sort, setSort] = useState({ column: null, direction: null });
-
-//   // Pagination State
-//   const [currentPage, setCurrentPage] = useState(1);
-//   const rowsPerPage = 10; // 1 page = 10 rows
-
-//   useEffect(() => {
-//     fetch("/data")
-//       .then((res) => res.json())
-//       .then((data) => setUsers(data));
-//   }, []);
-
-//   // Search Filter
-//   let filteredUsers = users.filter((user) =>
-//     user.name.toLowerCase().includes(search.toLowerCase())
-//   );
-
-//   // Sorting Logic
-//   if (sort.column) {
-//     filteredUsers = [...filteredUsers].sort((a, b) => {
-//       if (sort.direction === "asc") {
-//         return a[sort.column] > b[sort.column] ? 1 : -1;
-//       } else {
-//         return a[sort.column] < b[sort.column] ? 1 : -1;
-//       }
-//     });
-//   }
-
-//   function handleSort(column) {
-//     if (sort.column !== column) {
-//       setSort({ column, direction: "asc" });
-//     } else if (sort.direction === "asc") {
-//       setSort({ column, direction: "desc" });
-//     } else {
-//       setSort({ column: null, direction: null });
-//     }
-//   }
-
-//   // Pagination Calculation
-//   const totalPages = Math.ceil(filteredUsers.length / rowsPerPage);
-//   const indexOfLast = currentPage * rowsPerPage;
-//   const indexOfFirst = indexOfLast - rowsPerPage;
-//   const currentUsers = filteredUsers.slice(indexOfFirst, indexOfLast);
-
-//   function handlePrev() {
-//     if (currentPage > 1) setCurrentPage(currentPage - 1);
-//   }
-
-//   function handleNext() {
-//     if (currentPage < totalPages) setCurrentPage(currentPage + 1);
-//   }
-
-//   return (
-//     <div className="p-8">
-//       {/* Search Box */}
-//       <div className="mb-6 flex items-center gap-2 w-full max-w-xs border border-gray-300 px-3 py-2 rounded-md shadow-sm">
-//         <input
-//           type="text"
-//           placeholder="Search names.."
-//           value={search}
-//           onChange={(e) => {
-//             setCurrentPage(1); // jab search kare tab first page pr aa jaye
-//             setSearch(e.target.value);
-//           }}
-//           className="w-full outline-none text-sm"
-//         />
-
-//         {search && (
-//           <button onClick={() => setSearch("")} className="text-gray-500 hover:text-black">
-//             <X size={18} />
-//           </button>
-//         )}
-//       </div>
-
-//       <h2 className="text-2xl font-semibold mb-6 text-gray-800">Users Table</h2>
-
-//       <div className="overflow-x-auto shadow-lg rounded-lg border border-gray-200">
-//         <table className="w-full border-collapse text-sm">
-//           <thead className="bg-gray-100 text-left text-gray-600 font-semibold">
-//             <tr>
-//               <th onClick={() => handleSort("name")} className="px-4 py-3 border-b cursor-pointer">Name</th>
-//               <th className="px-4 py-3 border-b">Email</th>
-//               <th onClick={() => handleSort("age")} className="px-4 py-3 border-b cursor-pointer">Age</th>
-//               <th onClick={() => handleSort("role")} className="px-4 py-3 border-b cursor-pointer">Role</th>
-//             </tr>
-//           </thead>
-
-//           <tbody className="text-gray-700">
-//             {currentUsers.map((user) => (
-//               <tr key={user.id} className="hover:bg-gray-50 transition">
-//                 <td className="px-4 py-3 border-b">{user.name}</td>
-//                 <td className="px-4 py-3 border-b">{user.email}</td>
-//                 <td className="px-4 py-3 border-b">{user.age}</td>
-//                 <td className="px-4 py-3 border-b">{user.role}</td>
-//               </tr>
-//             ))}
-//           </tbody>
-//         </table>
-//       </div>
-
-//       {/* Pagination Buttons */}
-//       <div className="flex justify-between items-center gap-3 mt-4">
-
-//         <button
-//           onClick={handlePrev}
-//           disabled={currentPage === 1}
-//           className="px-4 py-2 border rounded-lg text-sm font-medium  cursor-pointer
-//                  hover:bg-gray-100 active:scale-95 transition disabled:opacity-50"
-//         >
-//           Prev
-//         </button>
-
-//         <span className="text-gray-600 text-sm">
-//           Page {currentPage} of {totalPages}
-//         </span>
-
-//         <button
-//           // onClick={handleNext}
-//           className="px-4 py-2 border rounded-lg text-sm font-medium cursor-pointer
-//                  hover:bg-gray-100 active:scale-95 transition disabled:opacity-50"
-//         >
-//           Manage Columns
-//         </button>
-
-//         <button
-//           onClick={handleNext}
-//           disabled={currentPage === totalPages}
-//           className="px-4 py-2 border rounded-lg text-sm font-medium cursor-pointer
-//                  hover:bg-gray-100 active:scale-95 transition disabled:opacity-50"
-//         >
-//           Next
-//         </button>
-
-
-//       </div>
-
-//     </div>
-//   ); 
-// }
-
 "use client";
 import { useEffect, useState } from "react";
 import { X } from "lucide-react";
@@ -170,7 +23,7 @@ export default function HomePage() {
 
   useEffect(() => {
     const saved = localStorage.getItem("visibleColumns");
-    setVisibleColumns(saved ? JSON.parse(saved) : ["name", "email", "age", "role"]);
+    setVisibleColumns(saved ? JSON.parse(saved) : ["name", "email", "age", "role","department","location"]);
   }, []);
 
   // Search Filter
@@ -216,7 +69,7 @@ export default function HomePage() {
     <div className="p-8">
 
       {/* Search Box */}
-      <div className="mb-6 flex items-center gap-2 w-full max-w-xs border border-gray-300 px-3 py-2 rounded-md shadow-sm">
+      <div className="mb-6 flex items-center gap-2 w-full max-w-xs border border-gray-300 px-3 py-2 rounded-full shadow-sm mx-auto">
         <input
           type="text"
           placeholder="Search names.."
@@ -235,7 +88,7 @@ export default function HomePage() {
         )}
       </div>
 
-      <h2 className="text-2xl font-semibold mb-6 text-gray-800">Users Table</h2>
+      <h2 className="text-4xl font-semibold mb-6 text-gray-800 text-center">Users Table</h2>
 
       <div className="overflow-x-auto shadow-lg rounded-lg border border-gray-200">
         <table className="w-full border-collapse text-sm">
@@ -253,6 +106,13 @@ export default function HomePage() {
               {visibleColumns.includes("role") && (
                 <th onClick={() => handleSort("role")} className="px-4 py-3 border-b cursor-pointer">Role</th>
               )}
+              {visibleColumns.includes("department") && (
+                <th onClick={() => handleSort("department")} className="px-4 py-3 border-b cursor-pointer">Department</th>
+              )}
+              {visibleColumns.includes("location") && (
+                <th onClick={() => handleSort("location")} className="px-4 py-3 border-b cursor-pointer">Location</th>
+              )}
+
             </tr>
           </thead>
 
@@ -263,6 +123,9 @@ export default function HomePage() {
                 {visibleColumns.includes("email") && <td className="px-4 py-3 border-b">{user.email}</td>}
                 {visibleColumns.includes("age") && <td className="px-4 py-3 border-b">{user.age}</td>}
                 {visibleColumns.includes("role") && <td className="px-4 py-3 border-b">{user.role}</td>}
+                    {visibleColumns.includes("department") && <td className="px-4 py-3 border-b">{user.department}</td>}
+               {visibleColumns.includes("location") && <td className="px-4 py-3 border-b">{user.location}</td>}
+           
               </tr>
             ))}
           </tbody>  
--- app/manageColumn/page.js
+++ app/manageColumn/page.js
@@ -1,98 +1,52 @@
-// "use client";
-// import { useState } from "react";
-// import Link from "next/link";
-
-// const allColumns = ["name", "email", "age", "role", "department", "location"];
-
-// export default function ManageColumnsPage() {
-//   const [selectedCols, setSelectedCols] = useState(() => {
-//     const saved = localStorage.getItem("visibleColumns");
-//     return saved ? JSON.parse(saved) : allColumns;
-//   });
-
-//   function toggleColumn(col) {
-//     let updated;
-//     if (selectedCols.includes(col)) {
-//       updated = selectedCols.filter((c) => c !== col);
-//     } else {
-//       updated = [...selectedCols, col];
-//     }
-//     setSelectedCols(updated);
-//     localStorage.setItem("visibleColumns", JSON.stringify(updated));
-//   }
-
-//   return (
-//     <div style={{ margin: "30px" }}>
-//       <h2>Manage Table Columns</h2>
-
-//       {allColumns.map((col) => (
-//         <div key={col}>
-//           <label>
-//             <input
-//               type="checkbox"
-//               checked={selectedCols.includes(col)}
-//               onChange={() => toggleColumn(col)}
-//             />
-//             {col}
-//           </label>
-//         </div>
-//       ))}
-
-//       <Link href="/home">
-//         <button style={{ marginTop: "20px" }}>Back</button>
-//       </Link>
-//     </div>
-//   );
-// }
-
 "use client";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
-const allColumns = ["name", "email", "age", "role"];
+export default function ManageColumn() {
+  const allColumns = ["name", "email", "age", "role", "department", "location"];
 
-export default function ManageColumnsPage() {
-  const [visibleColumns, setVisibleColumns] = useState([]);
+  const [selectedColumns, setSelectedColumns] = useState([]);
 
   useEffect(() => {
     const saved = localStorage.getItem("visibleColumns");
-    setVisibleColumns(saved ? JSON.parse(saved) : allColumns);
+    setSelectedColumns(saved ? JSON.parse(saved) : allColumns);
   }, []);
 
-  function toggleColumn(col) {
+  function handleToggle(column) {
     let updated;
-    if (visibleColumns.includes(col)) {
-      updated = visibleColumns.filter((c) => c !== col);
+    if (selectedColumns.includes(column)) {
+      updated = selectedColumns.filter((col) => col !== column);
     } else {
-      updated = [...visibleColumns, col];
+      updated = [...selectedColumns, column];
     }
-    setVisibleColumns(updated);
+
+    setSelectedColumns(updated);
     localStorage.setItem("visibleColumns", JSON.stringify(updated));
   }
 
   return (
     <div className="p-8">
-      <h2 className="text-xl font-semibold mb-4">Manage Table Columns</h2>
+      <h2 className="text-2xl font-semibold mb-6">Manage Columns</h2>
 
-      {allColumns.map((col) => (
-        <div key={col} className="mb-2">
-          <label className="flex items-center gap-2">
+      <div className="space-y-3">
+        {allColumns.map((col) => (
+          <label key={col} className="flex items-center gap-3 text-gray-700">
             <input
               type="checkbox"
-              checked={visibleColumns.includes(col)}
-              onChange={() => toggleColumn(col)}
+              checked={selectedColumns.includes(col)}
+              onChange={() => handleToggle(col)}
             />
             {col.toUpperCase()}
           </label>
-        </div>
-      ))}
-
-      <Link href="/home">
+        ))}
+         <Link href="/home">
         <button className="mt-4 px-4 py-2 border rounded hover:bg-gray-200">
-          Back to Table
-        </button>
-      </Link>
+           Back to Table
+         </button>
+       </Link>
+      </div>
     </div>
   );
 }
+
 
